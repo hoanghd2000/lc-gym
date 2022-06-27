f79ea612840b65dba4c020dc9477a53e04f75650
hoanghd2000 <hoanghd2000@gmail.com>
2022-06-27 11:52:24 +0700

    0003. Longest Substring Without Repeating Characters

---
+++ 0003. Longest Substring Without Repeating Characters/My_Solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        
+        if not s:
+            return 0
+        
+        char_set = {}
+        l = r = 0
+        lar_l = lar_r = 0
+        
+        while r < len(s):
+            if s[r] in char_set:
+                l = r = char_set[s[r]] + 1
+                char_set = {s[r]:r}
+            else:
+                char_set[s[r]] = r
+                if r - l > lar_r - lar_l:
+                    lar_l = l
+                    lar_r = r
+            r += 1
+        
+        return lar_r - lar_l + 1
