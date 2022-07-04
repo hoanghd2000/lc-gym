30102067ad8a1bb18419e2a60680442592c64f1e
hoanghd2000 <hoanghd2000@gmail.com>
2022-07-04 17:50:41 +0700

    0049. Group Anagrams

---
+++ 0049. Group Anagrams/Solution.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        # list of 26 prime numbers, each mapped to a English lowercase char
+        prime_no = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,81,83,89,97]
+        
+        res = {}
+        
+        # Processing and grouping for each str in strs
+        for str in strs:
+            prod = 1
+            for char in str:
+                prod *= prime_no[ord(char)-ord('a')]
+            if prod not in res:
+                res[prod] = [str]
+            else:
+                res[prod].append(str)
+        
+        return res.values()
