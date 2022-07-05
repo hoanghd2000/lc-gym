f8bdc1bfcdfe5962b6e687d94c3ad2d8a7a6341a
hoanghd2000 <hoanghd2000@gmail.com>
2022-07-05 23:46:01 +0700

    AltSol 0383. Ransom Note

---
+++ 0383. Ransom Note/AltSolution.py
@@ -0,0 +1,5 @@
+import collections
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        return not (collections.Counter(ransomNote) - collections.Counter(magazine))
