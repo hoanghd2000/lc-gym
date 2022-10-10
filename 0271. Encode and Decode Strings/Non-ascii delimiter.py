b5da5d96ca92ac48c76e3f73f6ec57f6d579f1bd
hoanghd2000 <hoanghd2000@gmail.com>
2022-10-10 13:36:35 +0800

    0271. Encode and Decode Strings - delimiter edge cases

--- 0271. Encode and Decode Strings/Non-ascii delimiter.py
+++ 0271. Encode and Decode Strings/Non-ascii delimiter.py
@@ -4,14 +4,18 @@ class Codec:
     def encode(self, strs: List[str]) -> str:
         """Encodes a list of strings to a single string.
         """
-        delimiter = 'ę'
-        return delimiter.join(strs)
+        if len(strs) == 0:
+            return chr(258)
+
+        return chr(257).join(strs)
 
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
         """
-        delimiter = 'ę'
-        return s.split(delimiter)
+        if s == chr(258):
+            return []
+
+        return s.split(chr(257))
 
 
 # Your Codec object will be instantiated and called as such:
